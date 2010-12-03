01c86e681e452dfc6326f23b8c70d770c64fbb30
Aaron DeBruin <debruin.aaron@gmail.com>
2010-12-03 15:10:54 -0500

    Up to version 15, hopefully fixing adb wireless and battery charging fix

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 16 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#16 PREEMPT Fri Dec 3 14:50:14 EST 2010"
+#define LINUX_COMPILE_TIME "14:50:14"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "Kubuntu-VM"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
