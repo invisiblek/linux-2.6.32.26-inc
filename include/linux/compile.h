f903d11f6e94213cf3cf83a88be8aa09224fe10c
Aaron DeBruin <debruin.aaron@gmail.com>
2010-12-31 02:12:41 -0500

    up to version 30, fixed syntax errors in cache patch, should fix KPs for some

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 30 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#16 PREEMPT Fri Dec 3 17:08:47 EST 2010"
-#define LINUX_COMPILE_TIME "17:08:47"
+#define UTS_VERSION "#30 PREEMPT Fri Dec 31 02:09:36 EST 2010"
+#define LINUX_COMPILE_TIME "02:09:36"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "Kubuntu-VM"
 #define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
