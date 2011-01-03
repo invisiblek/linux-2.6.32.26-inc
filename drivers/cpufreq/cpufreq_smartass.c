b55c2f2a7136db81320454f0c5962091ec5bbec0
Aaron DeBruin <debruin.aaron@gmail.com>
2011-01-02 20:56:40 -0500

    slightly more agressive underoltiving and more consistency in avs voltage tables, aded higher clock speeds for future experimenting

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -87,7 +87,8 @@ static unsigned int sleep_max_freq;
  * The frequency to set when waking up from sleep.
  * When sleep_max_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX
+//#define DEFAULT_SLEEP_WAKEUP_FREQ CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX
+#define DEFAULT_SLEEP_WAKEUP_FREQ 0
 static unsigned int sleep_wakeup_freq;
 
 /*
@@ -108,7 +109,7 @@ static unsigned int sample_rate_jiffies;
  * Freqeuncy delta when ramping up.
  * zero disables causes to always jump straight to max frequency.
  */
-#define DEFAULT_RAMP_UP_STEP 352000
+#define DEFAULT_RAMP_UP_STEP 128000
 static unsigned int ramp_up_step;
 
 /*
